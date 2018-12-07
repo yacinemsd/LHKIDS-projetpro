2e6138507bff7f3aee172ecbae7249cbdf7457d3
yacinemsd <messaoudi.y76@gmail.com>
2018-12-07 16:42:45 +0100

    lhkids

---
+++ lhkids/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>lh kids</title>
+    <link rel="stylesheet" href="style.css" />
+    <!-- materialize -->
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Galindo" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre" rel="stylesheet">
+  </head>
+    <body>
+      <header>
+    <!-- navbar   -->
+        <nav id="navbar">
+          <div class="">
+            <a href="#" class="brand-logo"><img class="brand-image" src="" />Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="">Je m'inscris !</a></li>
+              <li><a href="">Connexion</a></li>
+            </ul>
+          </div>
+        </nav>
+  <!-- fin navbar       -->
+  <div class="container-fluid">
+    <div class="row" id="fondHeader">
+      <div class="col s5 m4 l3">
+        <div class="collection" id="menu1">
+        <a href="#!" class="collection-item">Accueil</a>
+        <a href="#!" class="collection-item">Les Bon plans sorties !</a>
+        <a href="#!" class="collection-item">Je partage !</a>
+        <a href="#!" class="collection-item">Contact</a>
+      </div>
+    </div>
+    <div class="col s7 m 6 l9 ">
+      <h1 class="titreHeader1">LH KIDS</h1>
+      <h2 class="titreHeader2">Échange et partage de bons plans sorties au Havre pour vous et vos enfants !</h2>
+    </div>
+
+  </div>
+</div>
+    </header>
+
+
+    <script type="text/javascript" src="script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
