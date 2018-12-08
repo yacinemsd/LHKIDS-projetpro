2915a45c443954b00152e233964ce5decb0644dd
yacinemsd <messaoudi.y76@gmail.com>
2018-12-08 21:30:17 +0100

    fin css header

--- lhkids/index.php
+++ lhkids/index.php
@@ -5,23 +5,19 @@
     <title>lh kids</title>
     <link rel="stylesheet" href="style.css" />
     <!-- materialize -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+    <!-- fontawesom -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <!-- font -->
     <link href="https://fonts.googleapis.com/css?family=Galindo" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre" rel="stylesheet">
   </head>
     <body>
       <header>
     <!-- navbar   -->
-        <nav id="navbar">
-          <div class="">
-            <a href="#" class="brand-logo"><img class="brand-image" src="" />Logo</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-              <li><a href="">Je m'inscris !</a></li>
-              <li><a href="">Connexion</a></li>
-            </ul>
-          </div>
-        </nav>
+        <?php
+        require_once 'navbar.php'  // j'inclus a cet endroit mon fichier navbar.php contenant ma navbar
+         ?>
   <!-- fin navbar       -->
   <div class="container-fluid">
     <div class="row" id="fondHeader">
@@ -34,16 +30,22 @@
       </div>
     </div>
     <div class="col s7 m 6 l9 ">
-      <h1 class="titreHeader1">LH KIDS</h1>
-      <h2 class="titreHeader2">Échange et partage de bons plans sorties au Havre pour vous et vos enfants !</h2>
+        <h1 class="titreHeader1">LH KIDS</h1>
+        <h2 class="titreHeader2">Échange et partage de bons plans sorties au Havre pour vous et vos enfants !</h2>
+          <div id="buttons">
+            <a  id="buttonheader1" class="waves-effect waves-light btn"><i class="fas fa-user"></i>  S'inscrire !</a>
+            <a  id="buttonheader2" class="waves-effect waves-light btn"><i class="fas fa-sign-in-alt"></i>  Se connecter</a>
+          </div>
     </div>
 
   </div>
 </div>
     </header>
+<div class="">
 
-
+</div>
     <script type="text/javascript" src="script.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
   </body>
 </html>
---
+++ lhkids/navbar.php
@@ -0,0 +1,11 @@
+<!-- navbar   -->
+    <nav id="navbar">
+      <div class="">
+        <a href="#" class="brand-logo"><img class="brand-image" src="" />Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="">Je m'inscris !</a></li>
+          <li><a href="">Connexion</a></li>
+        </ul>
+      </div>
+    </nav>
+<!-- fin navbar       -->
