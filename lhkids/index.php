721bbaeb082eca5e90f00ab0609bc4d788cb2c7c
yacinemsd <messaoudi.y76@gmail.com>
2018-12-09 23:29:15 +0100

    cards presentation

--- lhkids/index.php
+++ lhkids/index.php
@@ -7,10 +7,13 @@
     <!-- materialize -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
     <!-- fontawesom -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <!-- font -->
     <link href="https://fonts.googleapis.com/css?family=Galindo" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Slackey" rel="stylesheet">
   </head>
     <body>
       <header>
@@ -24,7 +27,7 @@
       <div class="col s5 m6 l3">
         <div class="collection" id="menu1">
         <a href="#!" class="collection-item">Accueil</a>
-        <a href="#!" class="collection-item">Les Bon plans sorties !</a>
+        <a href="#!" class="collection-item">Les Bons plans sorties !</a>
         <a href="#!" class="collection-item">Je partage !</a>
         <a href="#!" class="collection-item">Contact</a>
       </div>
@@ -41,23 +44,78 @@
   </div>
 </div>
     </header>
+
     <!-- presentation -->
-<div class="container">
+<div class="container-fluid">
   <div class="row">
     <div class="col s12">
-      <div class="center-align presentation1 ">
-         <h1 class="center-align" id="title1">LH Kids ?</h1>
+      <div class="center-align presentation1">
+         <h1 class="center-align" id="title1">LH Kids, c'est quoi ?</h1>
          <p id="text1">LH kids, le site des parents pour les enfants a pour but de vous proposer les meilleurs plans sorties à faire avec vos enfants au Havre et ses alentours.<br>
                   La selection de sorties LH kids est faite par vous ! LH Kids c'est des parents, des grands-parents, des nounous, tontons, marraines… qui partagent et commentent leurs bons plans. Autant dire que ce sont les meilleurs, et les plus adaptées pour vos enfants !</p>
       </div>
-
     </div>
-
   </div>
+</div>
 
+<div class="container">
+  <div class="row">
+    <div class="col s12">
+      <div class="center-align">
+        <h1 class="center-align" id="title1">Comment ça marche ?</h1>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col s12 l4">
+      <div class="card center-align" id="card1">
+        <span class="card-title titlecard">1. Je m'inscris ! </span>
+        <div class="card-image imgcard">
+          <img src="images/inscription.jpg">
+        </div>
+        <div class="card-content cardContent">
+          <p>Simple, rapide et gratuite l'inscription vous permettra d'avoir votre compte LH Kids</p>
+        </div>
+        <div class="card-action" id="cardLink">
+          <a href="#">Je m'inscris !</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 l4">
+      <div class="card center-align" id="card2">
+        <span class="card-title titlecard">2. Je me connecte !</span>
+        <div class="card-image imgcard">
+          <img src="images/connexion.png">
+        </div>
+        <div class="card-content cardContent">
+          <p>Accéder à votre compte LH Kids, consulter le contenu partagé par la communauté LH Kids</p>
+        </div>
+        <div class="card-action" id="cardLink">
+          <a href="#">Me connecter</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 l4">
+      <div class="card center-align" id="card3">
+        <span class="card-title titlecard">3. Je partage !</span>
+        <div class="card-image imgcard">
+          <img src="images/share.png">
+        </div>
+        <div class="card-content cardContent">
+          <p>À votre tour partagez vos sorties et enrichissez le catologue de bons plans LH Kids!</p>
+        </div>
+        <div class="card-action" id="cardLink">
+          <a href="#">Je partage !</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
     <script type="text/javascript" src="script.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script src="http://materializecss.com/js/init.js"></script>
   </body>
 </html>
--- lhkids/navbar.php
+++ lhkids/navbar.php
@@ -2,10 +2,15 @@
     <nav id="navbar">
       <div class="">
         <a href="#" class="brand-logo"><img class="logoNav" src="images/logo.png" /></a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <li><a href="">Je m'inscris !</a></li>
           <li><a href="">Connexion</a></li>
         </ul>
       </div>
     </nav>
+    <ul class="sidenav" id="mobile-demo">
+      <li><a href="#">Je m'inscris !.html</a></li>
+      <li><a href="#">Connexion</a></li>
+    </ul>
 <!-- fin navbar       -->
