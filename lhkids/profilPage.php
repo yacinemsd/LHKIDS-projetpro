fa0717cc7e5854e3305064a394b0a69e9cdf881b
yacinemsd <messaoudi.y76@gmail.com>
2018-12-23 21:14:44 +0100

    profil page

---
+++ lhkids/profilPage.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Profil</title>
+
+    <link rel="stylesheet" href="style.css" />
+    <!-- materialize -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+    <!-- fontawesom -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Galindo" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Slackey" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <!-- navbar profil-->
+          <nav id="navbar2">
+            <div class="">
+              <a href="#" class="brand-logo"><img class="logoNav" src="images/logo.png" /></a>
+              <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="#">Les sorties!</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="connexion.php">Déconnexion</a></li>
+              </ul>
+            </div>
+          </nav>
+          <ul class="sidenav" id="mobile-demo">
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="#">Les sorties!</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="connexion.php">Déconnexion</a></li>
+          </ul>
+      <!-- fin navbar profil -->
+      <!-- profile-page-header -->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col s12">
+          <img class="profilBackground" src="images/fun.jpeg" />
+        </div>
+      </div>
+    </div>
+
+    </div>
+      <!--/ profile-page-header -->
+
+    </header>
+
+
+<!-- scripts -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
